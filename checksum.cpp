8c078223a175bd4ebbc1ce96ebafe96060559e4a
rajuislob <120424506+rajuislob@users.noreply.github.com>
2022-12-13 07:27:47 +0530

    Update checksum.cpp

--- checksum.cpp
+++ checksum.cpp
@@ -1,116 +1,126 @@
+
 #include <bits/stdc++.h>
 using namespace std;
-  
-// This function adds two binary strings and return
-// result as a third string
-string addBinary(string A, string B)
+
+string Ones_complement(string data)
 {
-    // If the length of string A is greater than the length
-    // of B then just swap the string by calling the
-    // same function and make sure to return the function
-    // otherwise recursion will occur which leads to
-    // calling the same function twice
-    if (A.length() > B.length())
-        return addBinary(B, A);
-  
-    // Calculating the difference between the length of the
-    // two strings.
-    int diff = B.length() - A.length();
-  
-    // Initialise the padding string which is used to store
-    // zeroes that should be added as prefix to the string
-    // which has length smaller than the other string.
-    string padding;
-    for (int i = 0; i < diff; i++)
-        padding.push_back('0');
-  
-    A = padding + A;
-    string res;
-    char carry = '0';
-  
-    for (int i = A.length() - 1; i >= 0; i--) {
-        // This if condition solves 110 111 possible cases
-        if (A[i] == '1' && B[i] == '1') {
-            if (carry == '1')
-                res.push_back('1'), carry = '1';
-            else
-                res.push_back('0'), carry = '1';
-        }
-        // This if condition solves 000 001 possible cases
-        else if (A[i] == '0' && B[i] == '0') {
-            if (carry == '1')
-                res.push_back('1'), carry = '0';
-            else
-                res.push_back('0'), carry = '0';
-        }
-        // This if condition solves 100 101 010 011 possible
-        // cases
-        else if (A[i] != B[i]) {
-            if (carry == '1')
-                res.push_back('0'), carry = '1';
-            else
-                res.push_back('1'), carry = '0';
-        }
-    }
-  
-    // If at the end there is carry then just add it to the
-    // result
-    if (carry == '1')
-        res.push_back(carry);
-    // reverse the result
-    reverse(res.begin(), res.end());
-  
-    // To remove leading zeroes
-    int index = 0;
-    while (index + 1 < res.length() && res[index] == '0')
-        index++;
-    return (res.substr(index));
+	for (int i = 0; i < data.length(); i++) {
+		if (data[i] == '0')
+			data[i] = '1';
+		else
+			data[i] = '0';
+	}
+	return data;
 }
+
+string checkSum(string data, int block_size)
+{
+	int n = data.length();
+	if (n % block_size != 0) {
+		int pad_size = block_size - (n % block_size);
+		for (int i = 0; i < pad_size; i++) {
+			data = '0' + data;
+		}
+	}
+	string result = "";
+	for (int i = 0; i < block_size; i++) {
+		result += data[i];
+	}
+	for (int i = block_size; i < n; i += block_size) {
+		string next_block = "";
+		for (int j = i; j < i + block_size; j++) {
+			next_block += data[j];
+		}
+		string additions = "";
+		int sum = 0, carry = 0;
+		for (int k = block_size - 1; k >= 0; k--) {
+			sum += (next_block[k] - '0')
+				+ (result[k] - '0');
+			carry = sum / 2;
+			if (sum == 0) {
+				additions = '0' + additions;
+				sum = carry;
+			}
+			else if (sum == 1) {
+				additions = '1' + additions;
+				sum = carry;
+			}
+			else if (sum == 2) {
+				additions = '0' + additions;
+				sum = carry;
+			}
+			else {
+				additions = '1' + additions;
+				sum = carry;
+			}
+		}
+
+		string final = "";
+
+		if (carry == 1) {
+			for (int l = additions.length() - 1; l >= 0;
+				l--) {
+				if (carry == 0) {
+					final = additions[l] + final;
+				}
+				else if (((additions[l] - '0') + carry) % 2
+						== 0) {
+					final = "0" + final;
+					carry = 1;
+				}
+				else {
+					final = "1" + final;
+					carry = 0;
+				}
+			}
+
+			result = final;
+		}
+		else {
+			result = additions;
+		}
+	}
+
+	return Ones_complement(result);
+}
+
+bool checker(string sent_message,
+			string rec_message,
+			int block_size)
+{
+
+	string sender_checksum
+		= checkSum(sent_message, block_size);
+	string receiver_checksum = checkSum(
+		rec_message + sender_checksum, block_size);
+	if (count(receiver_checksum.begin(),
+			receiver_checksum.end(), '0')
+		== block_size) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+// Driver Code
 int main()
 {
-	int dl;
-	int Cl;
-	string dataword="";
-	
-	cout<<"enter dataword\n";
-	cin>>dataword;
-	cout<<"length of cheksum \n";
-	cin>>Cl;
-	string *temp,t1="";
-	dl=dataword.length();
-	if(dl%Cl==0)
-	{
-		t1=dataword;
-		temp=new string[dl/Cl];
+	string sent_message
+		= "10000101011000111001010011101101";
+	string recv_message
+		= "10000101011000111001010011101101";
+	int block_size = 8;
+
+	if (checker(sent_message,
+				recv_message,
+				block_size)) {
+		cout << "No Error";
 	}
-	else
-	{	
-		for(int i=0;i<Cl-(dl%Cl);i++)
-		{
-			t1+="0";
-		}
-		t1+=dataword;
-		temp=new string[dl/Cl+1];
+	else {
+		cout << "Error";
 	}
-	for(int i=0;i<t1.length()/Cl;i++)
-	{
-		temp[i]="";
-	}
-	int nt =0;
-	while(nt<t1.length()/Cl)
-	{
-		for(int i=nt*Cl;i<(nt+1)*Cl;i++)
-		{
-			temp[i]+=t1[i];
-		}
-		nt++;
-	}
-	nt=1;
-	string t2="";
-	t2=temp[0];
-	while(nt<t1.length()/Cl)
-	{
-		t2=addBinary(t2,temp[nt]);
-		nt++;
-	}
+
+	return 0;
 }
+
